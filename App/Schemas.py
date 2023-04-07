e0d8c15faddbd7580065f300e1200139052b2d6f
Jish123K <128099655+Jish123K@users.noreply.github.com>
2023-04-07 22:33:46 +0530

    Update Schemas.py

--- App/Schemas.py
+++ App/Schemas.py
@@ -255,6 +255,30 @@ paid = Column(Boolean, default=False)
 created_at = Column(DateTime, default=datetime.utcnow)
 
 items = relationship("InvoiceItem", back_populates="invoice")
+id = Column(Integer, primary_key=True, index=True)
+
+username = Column(String, unique=True, index=True)
+
+email = Column(String, unique=True, index=True)
+
+hashed_password = Column(String)
+
+created_at = Column(DateTime, default=datetime.utcnow)
+
+invoices = relationship("Invoice", back_populates="owner")
+
+def __repr__(self):
+
+    return f"<User {self.username}>"
+
+def __str__(self):
+
+    return self.__repr__()
+
+def __unicode__(self):
+
+    return self.__repr__()
+
 
 
 
