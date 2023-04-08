2001949c41fae8df868732351284ebe89c05fe21
Jish123K <128099655+Jish123K@users.noreply.github.com>
2023-04-08 14:11:40 +0530

    Create Confog.py

---
+++ App/Confog.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_hostname = os.getenv("database_hostname")
+
+database_port = os.getenv("database_port")
+
+database_password = os.getenv("database_password")
+
+database_name = os.getenv("database_name")
+
+database_username = os.getenv("database_username")
+
+secret_key = os.getenv("secret_key")
+
+algorithm = os.getenv("algorithm")
+
+access_token_expire_minutes = int(os.getenv("access_token_expire_minutes"))
+
